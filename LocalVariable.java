0c5a41543dbed446db2b98fa55bde55a58eb30e6
Gollapalli shivateja <162829356+Gollapallishivateja@users.noreply.github.com>
2025-07-09 15:58:53 +0530

    Add files via upload

---
+++ LocalVariable.java
@@ -0,0 +1,24 @@
+public class LocalVariable{
+public static void main(String args[])
+{
+byte b=10;
+short s=20;
+int n=30;
+long l=40;
+float f=12.5f;
+double d=123.98;
+char c='S';
+boolean bb=false;
+String s1="shiva";
+System.out.println(b);
+System.out.println(s); 
+System.out.println(n);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(c);
+System.out.println(bb);
+System.out.println(s1);
+}
+}
+
